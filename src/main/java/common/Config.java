e547b9395701eab4f2b6bbe313db77cb68f5ed44
deniskislyuk <deniskislyuk@gmail.com>
2026-07-16 10:15:03 +0300

    feat: set browser to firefox

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -1,6 +1,6 @@
 package common;
 public class Config {
-    public static final String PLATFORM_AND_BROWSER = "win_chrome";
+    public static final String PLATFORM_AND_BROWSER = "win_";
     public static final Boolean CLEAR_COOKIES_AND_STORAGE = true;
     public static final String TEST_URL = "https://systeme.io/blog/cost-of-online-course";
     public static final String DB_URL = System.getProperty("db.url", "jdbc:postgresql://localhost:5432/your_db");
